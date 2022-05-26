2b7cc8ed8544a689200380af5a8ec68376f9c4d8
urmila4462 <dasari.urmila4462@gmail.com>
2022-05-26 12:27:19 +0530

    Display message

---
+++ GamblerSimulation/src/com/bridgelabz/gambler/Gambler.java
@@ -0,0 +1,31 @@
+package com.bridgelabz.gambler;
+
+public class Gambler {
+    public static void main(String[] args) {
+
+        System.out.println("Gambler simulation program");
+        int stake = 100;
+        int betPrice = 1;
+
+    }
+}
+
+   /* public boolean winOrLoss(){
+        if(Math.random()<0.5)
+            return true;
+        else
+            return false;
+    }
+
+    public void playGame(){
+        numberOfPlay++;
+        if(winOrloss())
+            stack+=betPrice;
+        else
+            stack-=betPrice;
+        if(stack<=0 || stack>=goal)
+            return;
+        else
+            playGame();
+    }
+}*/
